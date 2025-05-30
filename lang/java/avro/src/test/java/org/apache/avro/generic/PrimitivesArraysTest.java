e967b349c7b6aade5ca446f5cec0d044a511305a
rdifrango <ron.difrango@gmail.com>
2025-05-30 16:18:35 +0200

    AVRO-4110: Double Values Fail when using Min/Max (#3292)
    (cherry picked from commit b8a3655fef9901e3d043e830f6ff6fb5f543fda7)

--- lang/java/avro/src/test/java/org/apache/avro/generic/PrimitivesArraysTest.java
+++ lang/java/avro/src/test/java/org/apache/avro/generic/PrimitivesArraysTest.java
@@ -290,4 +290,18 @@ class PrimitivesArraysTest {
     Assertions.assertEquals(Double.MAX_VALUE, doubleArray.get(101));
     Assertions.assertEquals(Double.MIN_VALUE, doubleArray.get(100));
   }
+
+  @Test
+  void testDoubleArrayPreservesPrecisionForNonFloatRepresentableValues() {
+    final PrimitivesArrays.DoubleArray doubleArray = new PrimitivesArrays.DoubleArray(1,
+        Schema.createArray(Schema.create(Schema.Type.DOUBLE)));
+
+    // This value cannot be represented as a float
+    Double nonFloatDouble = .9;
+    Assertions.assertNotEquals(.9, nonFloatDouble.floatValue());
+
+    // Assert that the double array does not lose precision when adding
+    doubleArray.add(nonFloatDouble);
+    Assertions.assertEquals(nonFloatDouble, doubleArray.get(0));
+  }
 }
